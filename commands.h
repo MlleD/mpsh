1384528ab154f9403f486a3d4361dca675000c9d
DorotheeHuynh <dorothee.huynh@gmail.com>
2019-01-03 13:40:30 +0100

    Retrait fonctions

--- commands.h
+++ commands.h
@@ -5,10 +5,4 @@
 #include "var.h"
 #include "constantes.h"
 
-int ls (char *path);
-int mkdir (char* cmdargs[]);
-int cat (char* cmdargs[]);
-int echo (char* cmdargs[]);
-int del (char* cmdargs[]);
-
 int process_command (char * cmdargs[]);
